84fb0909b48e8192ca0ba3a4cc5fcb3be9da62da
angon1 <kubabak@gmail.com>
2021-01-07 14:24:00 +0100

    black test

--- app/excercises/models.py
+++ app/excercises/models.py
@@ -3,24 +3,30 @@ from app import db
 from sqlalchemy_serializer import SerializerMixin
 
 
-excerciseToTag = db.Table('excercisetotag',
-    db.Column('excercise_id', db.Integer, db.ForeignKey('excercise.id'), primary_key=True),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+excerciseToTag = db.Table(
+    "excercisetotag",
+    db.Column(
+        "excercise_id", db.Integer, db.ForeignKey("excercise.id"), primary_key=True
+    ),
+    db.Column("tag_id", db.Integer, db.ForeignKey("tag.id"), primary_key=True),
 )
 
 
-
 class Excercise(db.Model, SerializerMixin):
-#fields
+    # fields
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(512), unique=True)
     movieLink = db.Column(db.String(512))
-#relations
-    tags = db.relationship('Tag', secondary=excerciseToTag, lazy=True, back_populates="excercise")
-#methods
+    # relations
+    tags = db.relationship(
+        "Tag", secondary=excerciseToTag, lazy=True, back_populates="excercise"
+    )
+    # methods
     def __repr__(self):
-        return '<\nExcercise name: {}\n Description: {}\n Link: {}\n Tags: {}>'.format(self.name, self.description, self.movieLink, self.tagsDict())
+        return "<\nExcercise name: {}\n Description: {}\n Link: {}\n Tags: {}>".format(
+            self.name, self.description, self.movieLink, self.tagsDict()
+        )
 
     def addTag(self, tag):
         self.tags.append(tag)
@@ -33,7 +39,7 @@ class Excercise(db.Model, SerializerMixin):
         db.session.commit()
         return True
 
-    def removeTag(self,tag):
+    def removeTag(self, tag):
         if tag in self.tags:
             self.tags.remove(tag)
             db.session.commit()
@@ -48,28 +54,31 @@ class Excercise(db.Model, SerializerMixin):
         return tagDict
 
     def asDict(self):
-        excerciseDict = self.to_dict(rules=('-tags.excercise',))
+        excerciseDict = self.to_dict(rules=("-tags.excercise",))
         return excerciseDict
 
     def asDictNoTags(self):
-        excerciseDict = self.to_dict(rules=('-tags',))
+        excerciseDict = self.to_dict(rules=("-tags",))
         return excerciseDict
 
+
 class Tag(db.Model, SerializerMixin):
-#fields
+    # fields
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), index=True, unique=True)
     category = db.Column(db.String(512))
-#relations
-    excercise = db.relationship('Excercise', secondary=excerciseToTag, lazy=True, back_populates="tags")
-#methods
+    # relations
+    excercise = db.relationship(
+        "Excercise", secondary=excerciseToTag, lazy=True, back_populates="tags"
+    )
+    # methods
     def __repr__(self):
-        return '{}'.format(self.asDict())
+        return "{}".format(self.asDict())
 
     def asDict(self):
-        return {'id':self.id, 'name': self.name, 'category':self.category}
+        return {"id": self.id, "name": self.name, "category": self.category}
 
-    def addExcercise(self,excercise):
+    def addExcercise(self, excercise):
         self.excercise.append(excercise)
         db.session.commit()
         return True
